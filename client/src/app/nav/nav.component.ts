d4d019a7c7c78a902ec007cdd691a2778b524d58
洪啟睿 <y6602016@gmail.com>
2022-01-06 15:58:24 -0500

    add toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -14,7 +15,7 @@ export class NavComponent implements OnInit {
 
   // we set it public since template file directly use accountService to access
   // currentUser$, template file will auto sub/unsub the obervables
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   // to pwersist user after logging, we monitor currentUser observable in account service
   ngOnInit(): void {
@@ -25,14 +26,17 @@ export class NavComponent implements OnInit {
     // service's login method contains http post method, it returns a observable
     // it's lazy, so we use subscribe. the response will be the UserDto
     this.accountService.login(this.model).subscribe(response => {
+      // navigate to member page once login
       this.router.navigateByUrl('/members');
     }, error=> { // catch error here, ex: invalid username, which is defined in AccountController
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
+    // navigate to home page once logout
     this.router.navigateByUrl('/');
   }
 
