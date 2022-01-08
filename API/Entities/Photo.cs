0c18ad0ec8667ab9f685b891ddff8d40c2cecd00
洪啟睿 <y6602016@gmail.com>
2022-01-07 21:43:51 -0500

    update user schema

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,11 @@
+// we define schema in entity class file
+namespace API.Entities
+{
+  public class Photo
+  {
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string PublicId { get; set; }
+  }
+}
