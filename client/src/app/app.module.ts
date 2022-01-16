50b117b31e8a439ab7d80d801c1b4850f7657022
洪啟睿 <y6602016@gmail.com>
2022-01-16 16:08:03 -0500

    password confirm

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -49,6 +49,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule,
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,22 +14,48 @@ export class RegisterComponent implements OnInit {
 
   // use @Output to send properties from parent component
   @Output() cancelRegister = new EventEmitter();
-
-
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      // this.matchValue('password'), password here correspond to below matchTo for controls[matchTo]
+      confirmPassword: new FormControl('', [Validators.required, this.matchValue('password')])
+    })
+    // when password is typed and changed, confirmPassword need to update and validate it again
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  // return a validation function
+  matchValue(matchTo: string) : ValidatorFn {
+    // formControl derive from AbstractControl
+    return (control: AbstractControl) => {
+      // if password match, then return null, 
+      // otherwise attach a validator error called "isMatching" to the control, and it will fail the validation
+      return control?.value === control?.parent?.controls[matchTo].value 
+      ? null : {isMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    // })
   }
 
   // send value to parent component
