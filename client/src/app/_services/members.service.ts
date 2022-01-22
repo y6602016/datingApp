9de38c843caaef70db85fa50bf057a5712ef8219
洪啟睿 <y6602016@gmail.com>
2022-01-21 21:11:55 -0500

    add lists

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -144,6 +144,7 @@ export class MembersService {
   }
 
   getLikes(predicate: string) {
-    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+    // return Partial<Member[]> so loadLikes() can work in lists.component
+    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
   }
 }
